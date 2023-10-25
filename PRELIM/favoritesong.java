4d7493a9f224b636b205ab5131693c1ea991283d
EldrinTrapa <148033211+EldrinTrapa@users.noreply.github.com>
2023-10-25 13:03:08 +0800

    Add files via upload

---
+++ PRELIM/favoritesong.java
@@ -0,0 +1,12 @@
+class favoritesong{ 
+  
+   public static void main(String[] args){ 
+   
+       System.out.println("Twenty_three pero tawag sa akin daddy, yeah");   
+       System.out.println("You know me when i hit that punani");      
+       System.out.println("Maganda na mukha pati arms tsaka body, yeah");      
+       System.out.println("But sorry I don't trust nobody");      
+
+   
+   }
+} 
